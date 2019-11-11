55a8a816922590cb78efb9ae3d5e3e51a859481c
guilhermearaujo14 <guilherme.araujo14@hotmail.com>
2019-11-11 16:21:51 -0300

    Primeiro envio de arquivos

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function AbrirFoto1()
+{
+   document.querySelector('img#fotos').style.width='400px';
+   document.querySelector('img#fotos').style.height='400px';
+}
+
+function AbrirFoto2()
+{
+   document.querySelector('img#foto').style.width='400px';
+   document.querySelector('img#foto').style.height='400px';
+}
+
+function fecharFoto1()
+{
+    document.querySelector('img#fotos').style.width='100px';
+    document.querySelector('img#fotos').style.height='100px';
+}
+
+function fecharFoto2() 
+{
+    document.querySelector('img#foto').style.width = '100px';
+    document.querySelector('img#foto').style.height = '100px';
+}
+
